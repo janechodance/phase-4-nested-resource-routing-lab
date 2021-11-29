2b9cf47322b71be6b2c62b64be7e1316850c77c1
Jane <janechodance@gmail.com>
2021-11-29 09:55:36 -0500

    commit tree e4cb31da9b612dcd25aa58f921ae12ac6c3f3190

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,31 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+     items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
-
+  def show
+    item = Item.find_by(id: params[:id])
+    if item
+      render json: item, include: :user
+    else
+      render json: { error: "Item not found" }, status: :not_found
+    end
+  end
+  def create
+    item = Item.create(item_params)
+    render json: item, status: :created
+  end
+private
+def item_params
+  params.permit(:name, :description, :price, :user_id)
+end
+def render_not_found_response(exception)
+  render json: { error: "#{exception.model} not found" }, status: :not_found
+end
 end
